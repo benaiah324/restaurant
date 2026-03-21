b4e5eea58720069e363b004853847ba0030ced20
Benaiah Enebeli <258831278+benaiah324@users.noreply.github.com>
2026-03-21 10:34:41 +0100

    Created the menu page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import logoUrl from './assets/logo2.png'; 
 import createHome from './home.js';
+import createMenu from './menu.js';
 
 
 const content = document.getElementById('content');
